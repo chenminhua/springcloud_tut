653f2789ee6a0f7f6e165a088117af3f150d13f3
chenminhua <chenmh@blacklake.cn>
2019-02-13 22:46:28 +0800

    config

--- config-service/src/main/java/com/chenminhua/config/ConfigApplication.java
+++ config-service/src/main/java/com/chenminhua/config/ConfigApplication.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+/**
+ * 访问 http://localhost:8888/reservation/master
+ */
+
 @EnableConfigServer
 @SpringBootApplication
 public class ConfigApplication {
--- reservation/src/main/java/com/chenminhua/reservation/ReservationApp.java
+++ reservation/src/main/java/com/chenminhua/reservation/ReservationApp.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.annotation.Id;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
@@ -16,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import java.util.Collection;
 import java.util.stream.Stream;
 
@@ -37,6 +37,9 @@ public class ReservationApp {
 }
 
 
+/**
+ *  curl -d{} http://localhost:8000/actuator/refresh
+ */
 
 @RefreshScope
 @RestController
